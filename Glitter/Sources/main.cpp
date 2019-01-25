eca39becfba0e6d93a28a25a980b24b4bfbb590f
Syaiful Nizam Yahya <syaifulnizamyahya@gmail.com>
2019-01-25 10:51:25 +0800

    Flip and changed shader filename

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -47,7 +47,7 @@ int main()
 
 	// build and compile our shader program
 	// ------------------------------------
-	Shader ourShader("..\\Glitter\\Sources\\3.3.shader.vs", "..\\Glitter\\Sources\\3.3.shader.fs"); // you can name your shader files however you like
+	Shader ourShader("..\\Glitter\\Sources\\3.3.shader.vert", "..\\Glitter\\Sources\\3.3.shader.frag"); // you can name your shader files however you like
 	//Shader ourShader("3.3.shader.vs", "3.3.shader.fs"); // you can name your shader files however you like
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
